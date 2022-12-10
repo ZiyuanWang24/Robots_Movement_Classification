8f6036601a30364210f98ec6cc9cc4371652d8df
Yichen Yuan <yichen6@uw.edu>
2022-12-10 11:04:52 -0800

    knn function

---
+++ src/knn.py
@@ -0,0 +1,30 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import sklearn
+from sklearn.ensemble import RandomForestClassifier
+from sklearn import datasets
+import pickle
+
+def knnclassifier(datasets):
+    X = datasets.iloc[:, [0,1,2,3]].values
+    y = datasets.iloc[:, -1].values
+    from sklearn.preprocessing import LabelEncoder
+    le = LabelEncoder()
+    X[:,0] = le.fit_transform(X[:,0])
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+    from sklearn.preprocessing import StandardScaler
+    sc = StandardScaler()
+    X_train = sc.fit_transform(X_train)
+    X_test = sc.transform(X_test)
+    from sklearn.neighbors import KNeighborsClassifier
+    classifier = KNeighborsClassifier(n_neighbors = 8, p = 2)
+    classifier.fit(X_train, y_train)
+    y_pred = classifier.predict(X_test)
+    from sklearn.metrics import confusion_matrix,accuracy_score
+    cm = confusion_matrix(y_test, y_pred)
+    ac = accuracy_score(y_test,y_pred)
+    print("knn accuracy = ", ac)
+    return y_pred
+    
