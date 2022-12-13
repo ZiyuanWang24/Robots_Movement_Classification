bd4be259d378634390d391a5fc380e03352972fd
Tianyue Gao <105677421+gao2t@users.noreply.github.com>
2022-12-12 22:52:47 -0800

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,5 @@
 from src.svm import SVM_learning
 from src.knn import knnclassifier
+from src.nnet import NNetclassifier
+from src.nnet import Linclassifier
 from .Classifiers import robots_movement_classifier
